5b2eea018e9d78ff24c8bb98a397c05af2e28bce
dhuy00 <huyduc480@gmail.com>
2025-12-20 15:03:53 +0700

    coding UI for todo list, handle css drag and drop for task item

--- knowly_frontend/src/components/todolist/Task.tsx
+++ knowly_frontend/src/components/todolist/Task.tsx
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Date from "../common/Date";
 import eventBus from "../../utils/eventBus";
 import { EVENT_OPEN_TASK_DETAIL } from "../../constant/event";
+import { IoMenu } from "react-icons/io5";
 
 const Task = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [isDragOver, setIsDragOver] = useState(false);
+  const isDraggingStyle = 'bg-gray-200 opacity-50'
+  const isDragOverStyle = 'outline outline-2 outline-dashed outline-gray-400 border-transparent bg-gray-100'
+
   const openTaskDetail = () => {
-    eventBus.emit(EVENT_OPEN_TASK_DETAIL, true)
+    eventBus.emit(EVENT_OPEN_TASK_DETAIL, true);
+  };
+
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData("text/plain", "task-id-1");
+    setIsDragging(true);
+  };
+
+  const handleDragEnd = (e) => {
+    setIsDragging(false);
+    setIsDragOver(false);
+  };
+
+  const handleDragEnter = (e) => {
+    setIsDragOver(true)
   }
-  
+
+  const handleDragLeave = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setIsDragOver(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Is drag over: ", isDragOver)
+  }, [isDragOver])
+
   return (
-    <div className="min-h-20 rounded-lg border p-2 border-border-secondary flex flex-col justify-center gap-1">
-      <span className="font-medium hover:underline cursor-pointer" onClick={openTaskDetail}>Coding UI for banking system project</span>
-      <div className="flex gap-2 text-text-secondary">
-        <Date />
-        <span>-</span>
-        <Date />
+    <div className={`task-item min-h-20 rounded-lg border p-2 border-border-secondary 
+    flex justify-between items-center transition-all ${isDragging ? isDraggingStyle : ''} 
+    ${isDragOver ? isDragOverStyle : ''}`}
+    draggable 
+    onDragStart={handleDragStart} 
+    onDragEnd={handleDragEnd}
+    onDragLeave={handleDragLeave}
+    onDragEnter={handleDragEnter}
+    >
+      <div className="flex flex-col justify-center gap-1">
+        <span
+          className="font-medium hover:underline cursor-pointer"
+          onClick={openTaskDetail}
+        >
+          Coding UI for banking system project
+        </span>
+        <div className="flex gap-2 text-text-secondary">
+          <Date />
+          <span>-</span>
+          <Date />
+        </div>
+      </div>
+      <div>
+        <IoMenu className="text-[20px] text-text-secondary cursor-grab mr-2" />
       </div>
     </div>
   );
