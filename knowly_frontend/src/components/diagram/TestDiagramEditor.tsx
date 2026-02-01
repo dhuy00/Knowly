76f681f22009c33d42acfc66c7a8c1354696a08f
dhuy00 <huyduc480@gmail.com>
2026-02-01 19:18:07 +0700

    custom crow's foot notation

--- knowly_frontend/src/components/diagram/TestDiagramEditor.tsx
+++ knowly_frontend/src/components/diagram/TestDiagramEditor.tsx
@@ -1,55 +1,171 @@
-import React, { useCallback } from 'react';
-import {
-  ReactFlow,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-  Background,
-} from '@xyflow/react';
+import { ReactFlow, Background, MarkerType, BezierEdge } from "@xyflow/react";
 
-import '@xyflow/react/dist/style.css';
+import "@xyflow/react/dist/style.css";
+import CustomEdge from "../test/CustomEdge";
 
-import CustomNode from '../test/CustomNode';
-import ConnectionLine from '../test/ConnectionLine';
+const edgeTypes = {
+  default: BezierEdge,
+  custom: CustomEdge,
+};
 
-const initialNodes = [
+const defaultNodes = [
   {
-    id: 'connectionline-1',
-    type: 'custom',
-    data: { label: 'Node 1' },
-    position: { x: 250, y: 5 },
+    id: "A",
+    position: { x: 20, y: 20 },
+    data: { label: "A" },
+  },
+  {
+    id: "B",
+    position: { x: 100, y: 200 },
+    data: { label: "B" },
+  },
+  {
+    id: "C",
+    position: { x: 300, y: 20 },
+    data: { label: "C" },
+  },
+  {
+    id: "D",
+    position: { x: 300, y: 170 },
+    data: { label: "D" },
+  },
+  {
+    id: "E",
+    position: { x: 250, y: 300 },
+    data: { label: "E" },
+  },
+  {
+    id: "F",
+    position: { x: 250, y: 450 },
+    data: { label: "F" },
+  },
+  {
+    id: "G",
+    position: { x: 20, y: 450 },
+    data: { label: "G" },
+  },
+  {
+    id: "H",
+    position: { x: 500, y: 450 },
+    data: { label: "H" },
   },
 ];
 
-const nodeTypes = {
-  custom: CustomNode,
-};
+const defaultEdges = [
+  {
+    id: "A->B",
+    source: "A",
+    target: "B",
+    markerEnd: {
+      type: MarkerType.Arrow,
+    },
+    label: "default arrow",
+  },
+  {
+    id: "C->D",
+    source: "C",
+    target: "D",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+    label: "default closed arrow",
+  },
+  {
+    id: "D->E",
+    source: "D",
+    target: "E",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+    markerStart: {
+      type: MarkerType.ArrowClosed,
+      orient: "auto-start-reverse",
+    },
+    label: "marker start and marker end",
+  },
+  {
+    id: "E->F",
+    source: "E",
+    target: "F",
+    markerEnd: "logo",
+    label: "custom marker",
+  },
+  {
+    id: "E->H",
+    source: "E",
+    target: "H",
+    type: "custom",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 20,
+      height: 20,
+    },
+    label: "change color on selection",
+  },
+  {
+    id: "B->G",
+    source: "B",
+    target: "G",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 20,
+      height: 20,
+      color: "#FF0072",
+    },
+    label: "marker size and color",
+    style: {
+      strokeWidth: 2,
+      stroke: "#FF0072",
+    },
+  },
+];
 
-const ConnectionLineFlow = () => {
-  const [nodes, _, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [],
-  );
-
+export default function TestDiagramEditor() {
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      connectionLineComponent={ConnectionLine}
-      onConnect={onConnect}
-      fitView
-      fitViewOptions={{
-        padding: 0.2,
-      }}
-    >
-      <Background />
-    </ReactFlow>
+    <>
+      <div className="w-full h-screen">
+        <svg style={{ position: "absolute", top: 0, left: 0 }}>
+          <defs>
+            <marker
+              id="logo"
+              viewBox="0 0 10 10"
+              markerWidth={12}
+              markerHeight={12}
+              refX={10} // 🔥 điểm chạm vào edge
+              refY={5}
+              orient="auto"
+              markerUnits="strokeWidth"
+            >
+              {/* vertical line */}
+              <line
+                x1="7"
+                y1="1"
+                x2="7"
+                y2="9"
+                stroke="black"
+                strokeWidth="1"
+              />
+              {/* horizontal line */}
+              <line
+                x1="1"
+                y1="5"
+                x2="9"
+                y2="5"
+                stroke="black"
+                strokeWidth="1"
+              />
+            </marker>
+          </defs>
+        </svg>
+        <ReactFlow
+          defaultNodes={defaultNodes}
+          defaultEdges={defaultEdges}
+          fitView
+          edgeTypes={edgeTypes}
+        >
+          <Background />
+        </ReactFlow>
+      </div>
+    </>
   );
-};
-
-export default ConnectionLineFlow;
+}
