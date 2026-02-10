107ed19c27a08fea68d5da0c4413427b6461519d
dhuy00 <huyduc480@gmail.com>
2026-02-10 20:39:49 +0700

    update UI for toolbar diagram

---
+++ knowly_frontend/src/assets/icons/EdgeType.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const EdgeType = ({size = 13}) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 13 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="11.5" y1="2" x2="11.5" y2="7" stroke="black" />
+      <rect x="10.5" y="0.5" width="2" height="2" stroke="black" />
+      <rect x="0.5" y="10.5" width="2" height="2" stroke="black" />
+      <line x1="12" y1="6.5" x2="1" y2="6.5" stroke="black" />
+      <line x1="1.5" y1="10" x2="1.5" y2="6" stroke="black" />
+    </svg>
+  );
+};
+
+export default EdgeType;
--- knowly_frontend/src/components/diagram/DiagramToolbar.tsx
+++ knowly_frontend/src/components/diagram/DiagramToolbar.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import type { AddNodeFunction } from "../../hooks/useNode";
 import { IoLogoWebComponent } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
+import { MdOutlineTableChart } from "react-icons/md";
+import { RiStackLine } from "react-icons/ri";
+import Divider from "../common/Divider";
+import EdgeType from "../../assets/icons/EdgeType";
+import { IoIosArrowDown } from "react-icons/io";
 
 interface DiagramToolbarProp {
   addNode: AddNodeFunction;
@@ -13,7 +18,7 @@ const DiagramToolbar: React.FC<DiagramToolbarProp> = ({ addNode }) => {
   };
 
   return (
-    <div className="shadow-sm w-60 rounded-md flex flex-col overflow-hidden bg-background-primary px-3">
+    <div className="shadow-sm gap-2 w-62 rounded-md flex flex-col overflow-hidden bg-background-primary px-3">
       <div className="w-full text-sm font-semibold py-2 flex items-center gap-2">
         <span className="bg-indigo-600 text-white p-1 rounded-sm">
           <IoLogoWebComponent className="text-xl" />
@@ -24,9 +29,35 @@ const DiagramToolbar: React.FC<DiagramToolbarProp> = ({ addNode }) => {
         </div>
       </div>
       {/* Search bar */}
-      <div className="flex bg-[#F6F6F6] gap-2 justify-center items-center px-2 py-1 rounded-sm"> 
-        <IoSearchOutline className="text-xl"/>
-        <input type="text" className="w-full focus:outline-none" placeholder="Search"/>
+      <div className="flex bg-[#F6F6F6] gap-2 justify-center items-center px-2 py-1 rounded-sm border border-gray-300">
+        <IoSearchOutline className="text-xl" />
+        <input
+          type="text"
+          className="w-full focus:outline-none placeholder:text-small placeholder:font-semibold text-small text-text-secondary
+        font-semibold"
+          placeholder="Search"
+        />
+      </div>
+
+      <div className="flex items-center gap-2 py-1 px-2 hover:bg-stone-100 rounded-sm cursor-pointer">
+        <MdOutlineTableChart className="text-lg text-gray-600" />
+        <span className="text-common font-medium text-black">Table</span>
+      </div>
+
+      <div className="flex items-center gap-2 py-1 px-2 hover:bg-stone-100 rounded-sm cursor-pointer">
+        <RiStackLine className="text-lg text-gray-600" />
+        <span className="text-common font-medium text-black">Node</span>
+      </div>
+      <Divider />
+      <span className="text-small text-text-secondary font-medium">
+        Configuaration
+      </span>
+      <div className="flex">
+        <div className="border flex gap-2 justify-between border-stone-400 pl-2 pr-1 py-1
+        rounded-xs cursor-pointer">
+          <EdgeType size={15}/>
+          <IoIosArrowDown className="text-xs"/>
+        </div>
       </div>
     </div>
   );
