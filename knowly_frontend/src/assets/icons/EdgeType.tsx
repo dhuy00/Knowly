dc2e5293027f154c3763d0e0c019ff8fb366186d
dhuy00 <huyduc480@gmail.com>
2026-02-13 22:04:25 +0700

    random commit

--- knowly_frontend/src/assets/icons/EdgeType.tsx
+++ knowly_frontend/src/assets/icons/EdgeType.tsx
@@ -5,7 +5,7 @@ const EdgeType = ({size = 13}) => {
     <svg
       width={size}
       height={size}       
-      viewBox="0 0 13 13"
+      viewBox="0 0 13 13"     
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
