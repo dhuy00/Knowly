f1c7aa946832c1f1a0ff190a3692f6e23a8e93de
dhuy00 <huyduc480@gmail.com>
2026-04-14 20:29:16 +0700

    refactor - update size for component

--- knowly_frontend/src/components/my-task/CustomSelect.tsx
+++ knowly_frontend/src/components/my-task/CustomSelect.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { IoChevronDownOutline } from "react-icons/io5";
 
-const CustomSelect = () => {
+const CustomSelect = ({defaultValue, list}) => {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
 
   const handleDropdown = () => {
@@ -12,23 +12,22 @@ const CustomSelect = () => {
     <div className="relative">
       <div
         className={`bg-[#0F0F0F] border border-[#2A2A2A] rounded-lg text-gray-400 text-sm
-       px-4 py-2 outline-none focus:border-emerald-500/50 transition text-xs flex justify-between items-center
-       gap-6 relative`}
+       px-4 h-10 outline-none focus:border-emerald-500/50 transition text-xs flex justify-between items-center
+       gap-6 relative font-medium`}
        onClick={handleDropdown}
       >
-        <span>Last 7 days</span>
+        <span>{defaultValue}</span>
         <IoChevronDownOutline className={`text-[15px] transition ${isOpenDropdown ? 'rotate-180' : ''}`} />
       </div>
       <ul
         className={`bg-[#0F0F0F] border border-[#2A2A2A] rounded-lg text-gray-400 text-sm
-       transition text-xs flex flex-col absolute top-9 left-0 right-0 ${isOpenDropdown ? '' : 'hidden'}`}
+       transition text-xs flex flex-col absolute top-11 left-0 right-0 ${isOpenDropdown ? '' : 'hidden'}`}
       >
+        {list.map((item) => (
         <li className="hover:bg-gray-950 bg-[#0F0F0F] px-4 z-10 py-2 rounded-t-lg transition">
-          Last 30 days
+          {item}
         </li>
-        <li className="hover:bg-gray-950 bg-[#0F0F0F] z-10 px-4 py-2 rounded-b-lg transition">
-          Last 90 days
-        </li>
+        ))}
       </ul>
     </div>
   );
--- knowly_frontend/src/components/my-task/FilterBar.tsx
+++ knowly_frontend/src/components/my-task/FilterBar.tsx
@@ -6,35 +6,42 @@ const FilterBar = () => {
   const [filterStatus, setFilterStatus] = useState("all");
   const [filterPriority, setFilterPriority] = useState("all");
   const [searchTerm, setSearchTerm] = useState("");
+  const mockDataList = [
+    "option one",
+    "option two",
+    "option three",
+    "option four",
+    "option five"
+  ]
+
+  const mockPriority = [
+    "All Priority",
+    "Urgent",
+    "High",
+    "Medium",
+    "Low",
+  ]
 
   return (
     <div className="bg-[#1A1A1A] border border-[#2A2A2A]/50 rounded-md p-4">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <div className="relative">
-          <Search className="absolute left-3 top-[13px] size-4 text-gray-500" />
+          <Search className="absolute left-3 top-[12px] size-4 text-gray-500" />
           <input
             type="text"
             placeholder="Search tasks..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full pl-10 pr-4 py-2.5 bg-[#0F0F0F] border border-[#2A2A2A]/50 
+            className="w-full pl-10 pr-4 h-10 bg-[#0F0F0F] border border-[#2A2A2A]/50 
             rounded-md text-white placeholder-gray-600 outline-none 
             focus:border-emerald-500/50 transition text-sm"
           />
         </div>
-        <CustomSelect/>
-        <select
-          value={filterPriority}
-          onChange={(e) => setFilterPriority(e.target.value)}
-          className="px-4 py-2.5 bg-[#0F0F0F] border border-[#2A2A2A]/50 rounded-xl text-white outline-none focus:border-emerald-500/50 transition"
-        >
-          <option value="all">All Priority</option>
-          <option value="urgent">Urgent</option>
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
-        </select>
-        <button className="flex items-center justify-center gap-2 px-4 py-2.5 bg-[#0F0F0F] border border-[#2A2A2A]/50 rounded-xl text-gray-400 hover:text-white hover:border-emerald-500/30 transition">
+        <CustomSelect defaultValue="Last 7 days" list={mockDataList}/>
+        <CustomSelect defaultValue="All priority" list={mockPriority}/>
+        <button className="flex items-center text-xs justify-center gap-2 px-4 h-10
+        bg-[#0F0F0F] border border-[#2A2A2A]/50 rounded-md text-gray-400 
+        hover:text-white hover:border-emerald-500/30 transition">
           <Filter className="size-4" />
           More Filters
         </button>
